ac8eb0ff7d7f4def7bb8604d908362f5bbb719d1
Seth Shaw <seth.shaw@unlv.edu>
2017-08-14 16:22:16 -0700

    incomplete *backup*

--- migrate-locations-as/migrate_locations.py
+++ migrate-locations-as/migrate_locations.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
 import json
-import logging
 import urllib, urllib2
 import base64
-import ConfigParser
+import os, sys, ConfigParser, logging, csv
 
 TEST = True
 SESSION = ''
@@ -53,10 +52,44 @@ def archivesspace_login():
                                         'password') }))
     SESSION = obj["session"]
 
+
+def parse_containers(to_parse):
+    containers = []
+
+    #Clean-up input
+    to_parse = to_parse.partition(' of ')[0] #trim box count if provided
+    to_parse = to_parse.lower()
+    to_parse = to_parse.replace('boxes','') #Clean up and remove default "Boxes"
+    to_parse = to_parse.replace('box','') #Clean up and remove default "Boxes"
+    to_parse = to_parse.strip() #Clean up and remove default "Boxes"
+
+    for group in to_parse.split(','):
+        if '-' in group:
+            (start, part, end) = group.partition('-')
+            if start.strip().isdigit() and end.strip().isdigit():
+                containers.extend(range(int(start),int(end)+1))
+            else:
+                containers.append(group.strip())
+        elif 'to' in group:
+            (start, part, end) = group.partition('to')
+            if start.strip().isdigit() and end.strip().isdigit():
+                containers.extend(range(int(start),int(end)+1))
+            else:
+                containers.append(group.strip())
+        elif ' and ' in group:
+            containers.extend(group.split(' and '))
+        else:
+            containers.append(group.strip())
+
+    return containers
+
+
 if __name__ == '__main__':
 
+    # logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+    #                     filename='location-migration.log',level=logging.DEBUG)
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename='location-migration.log',level=logging.INFO)
+                        level=logging.INFO)
 
     global config
     config = ConfigParser.ConfigParser()
@@ -72,10 +105,10 @@ if __name__ == '__main__':
     page_size = 200
     while next_page <= last_page:
         location_obj = archivesspace_api_call('/locations', data={'page':next_page, 'page_size':page_size})
-        print("Page %s of %s" % (next_page, last_page))
+        # print("Page %s of %s" % (next_page, last_page))
         # print(json.dumps(location_obj))
         if location_obj['last_page'] != last_page:
-            print('Updating last page from %s to %s' % (last_page, location_obj['last_page']))
+            # print('Updating last page from %s to %s' % (last_page, location_obj['last_page']))
             last_page = location_obj['last_page']
         for location in location_obj['results']:
             coords = ''
@@ -94,6 +127,39 @@ if __name__ == '__main__':
     # print json.dumps(locations, sort_keys=True, indent=4)
     # EACH spreadsheet row
 
-    # Find the collection by MS
+    if len(sys.argv)<2:
+        sys.exit("Please provide locations spreadsheet.")
+    if os.path.isfile(sys.argv[1]) != True :
+        sys.exit('"'+sys.argv[1] + '" is not a file. Please provide the locations spreadsheet.')
 
-    # Create a top-level container
+    with open(sys.argv[1], 'rU') as csvfile:
+
+        reader = csv.DictReader(csvfile, dialect=csv.excel_tab)
+        for row in reader:
+            # print 'Room: %s\tArea: %s\tColl id: %s\tColl title: %s\tContainer: %s' % (row['Room'],row['Location'],row['Collection number'],row['Collection Title'],row['Container']) #Room	Location	Collection number	Collection Title	Container	Original Collection Title
+            # Find the collection by MS
+
+            # SET ASIDE WHILE WORKING ON OTHER CODE
+            # if not 'Location' in row.keys():
+            #     print "Location for this row not found"
+            # elif not 'Collection number' in row.keys():
+            #     print '%s has no collection number associated with it' % row['Location']
+            # elif row['Collection number'] == '':
+            #     print '%s appears empty' % (row['Location'])
+            # elif not row['Collection number'].startswith('MS'):
+            #     print '%s has something other than an MS: %s' % (row['Location'], row['Collection number'])
+            # else:
+            #     results_obj = archivesspace_api_call("/repositories/%s/search" % config.get('archivesspace', 'repository'),data={'q':row['Collection number'],'page':'1','page_size':'1'})
+            #     if not 'results' in results_obj.keys() or not results_obj['results'] or results_obj['results'][0]['identifier'] != row['Collection number']:
+            #         print '%s has %s but couldn\'t find it in AS:\t%s' % (row['Location'], row['Collection number'], parse_containers(row['Container']))
+            #     else:
+            #         print "%s has %s (%s):\t%s" % (row['Location'], results_obj['results'][0]['identifier'], results_obj['results'][0]['id'], parse_containers(row['Container']))
+
+            # Find Location URI in hash
+            location_code = format('%s %s') % (row['Room'].strip(),row['Location'].strip())
+            if location_code in locations.keys():
+                print 'Found location %s: %s' % (locations[location_code], location_code)
+            else:
+                print 'Couldn\'t find %s' % location_code
+            # Create a top-level container
+            # Create Instance
