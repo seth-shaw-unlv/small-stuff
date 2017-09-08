2de8227550426ce7a3ff90563f0874b1a2e323d7
Seth Shaw <seth.shaw@unlv.edu>
2017-09-08 15:31:57 -0700

    Use resource ids as args and add DC metadata support
    Script now takes resource identifiers (from their uri) as command-line arguments instead of a CSV of identifiers.
    
    Now uses Dublin Core as the EZID metadata profile, taking values from an ArchivesSpace API call.

--- as-coll-mint/as_coll_mint.py
+++ as-coll-mint/as_coll_mint.py
@@ -9,25 +9,28 @@ import os
 import sys
 import urllib, urllib2
 
-# as_coll_mint.py takes a spreadsheet of resources (must include id,
-# identifier; optionally include ead_location), mints an ARK, updates the
-# resources' ead_location, and exports a PDF.
-#
-# @author Seth Shaw
-# @date 2017-08-02
-#
-# Before running the configuration file must be complete.
-# Copy config.ini.example to config.ini and update the necessary values.
-#
-# Note that this script assumes all resources are from the same repository.
-# The repository id is set in the config.ini file.
+"""
+as_coll_mint.py takes ArchivesSpace resource identifiers (see their uri) as a
+series of arguments, mints an ARK, updates the resources' ead_location, and
+exports a PDF.
+
+@author Seth Shaw
+@date 2017-08-02
+@updated 2017-09-08: Use resource ids as args and add DC metadata support
+
+Before running the configuration file must be complete.
+Copy config.ini.example to config.ini and update the necessary values.
+
+Note that this script assumes all resources are from the same repository.
+The repository id is set in the config.ini file.
+"""
 
 SESSION = ''
 
 
 
 # who_what_when is a dict with the keys who, what, and when. All other keys are ignored.
-def mint_ark(identifier, who_what_when={}):
+def mint_ark(identifier, dublin_core={}):
     request = urllib2.Request("%s/%s" % (config.get('ezid','minter-url'),
                               config.get('ezid','ark-shoulder')))
     request.add_header("Content-Type", "text/plain; charset=UTF-8")
@@ -42,9 +45,8 @@ def mint_ark(identifier, who_what_when={}):
     target = "%s/%s.pdf" % (config.get('archivesspace','pdf-url-prefix'),
                             identifier )
     data = "_target: %s\n" % (target)
-    for descriptive_item in ('who', 'what', 'when'):
-        if descriptive_item in who_what_when.keys() and who_what_when[descriptive_item]:
-            data += '%s: %s\n' % (descriptive_item, who_what_when[descriptive_item])
+    for descriptive_item_term, descriptive_item_value in dublin_core.iteritems():
+        data += '%s: %s\n' % (descriptive_item_term, descriptive_item_value)
 
     request.add_data(data.encode("UTF-8"))
 
@@ -65,29 +67,6 @@ def mint_ark(identifier, who_what_when={}):
           response = e.fp.read()
           if not response.endswith("\n"): response += "\n"
           logging.error("Can't mint ark. Response: %s", response)
-
-def update_ead_location(id, location):
-    #pull JSON object for resource
-    resource = archivesspace_api_call(
-            '/repositories/%s/resources/%s' % (config.get('archivesspace',
-                                                          'repository'),id))
-
-    #update ead_location field if it doesn't already match
-    if (('ead_location' not in resource.keys())
-    or (location != resource['ead_location'])):
-        resource['ead_location'] = location #update the existing object
-        archivesspace_api_call('/repositories/%s/resources/%s' % (
-                    config.get('archivesspace', 'repository'),id),
-                'POST', json.dumps(resource))
-
-def export_fa_pdf(id, identifier):
-
-    if not os.path.isdir(config.get('pdf','export-location')):
-         #prep the path for exporting files
-        os.makedirs(os.path.normpath(config.get('pdf','export-location')))
-
-    with open(os.path.normpath('%s/%s.pdf' % (config.get('pdf','export-location'), identifier )), "wb") as local_file:
-        local_file.write(archivesspace_api_call("/repositories/%s/resource_descriptions/%s.pdf" % (config.get('archivesspace', 'repository'), id ), as_obj = False).read())
 
 def archivesspace_api_call(path, method = 'GET', data = {}, as_obj = True):
 
@@ -136,91 +115,63 @@ def archivesspace_login():
 if __name__ == '__main__':
 
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename='as-coll-mint.log',level=logging.INFO)
+                        level=logging.INFO)
 
     global config
     config = ConfigParser.ConfigParser()
     configFilePath = r'config.ini'
     config.read(configFilePath)
 
-    if len(sys.argv)<2:
-        sys.exit("Please provide a TSV with id and identifer columns.")
-    if os.path.isfile(sys.argv[1]) != True :
-        sys.exit('"'+sys.argv[1] + '" is not a file. Please provide a TSV with id and identifer columns.')
+for rid in sys.argv[1:]:
+    logging.info("Attempting to Mint an ARK for %s" % rid)
+    #sanity Check
+    if not rid.isdigit():
+        logging.warning("Not a digit (can't be a resource identifier): %s" % rid)
+        continue
 
-    with open(sys.argv[1]) as csvfile:
+    resource = archivesspace_api_call('/repositories/%s/resources/%s' % (config.get('archivesspace','repository'),rid))
+    logging.debug('RESOURCE JSON FOR %s: %s' % (rid, json.dumps(resource)))
+    if not resource:
+        continue
+    elif 'error' in resource.keys():
+        logging.warning("Could not find a resource for %s: %s" % (rid,resource['error']))
+        continue
 
-        reader = csv.DictReader(csvfile, delimiter='\t')
-        for row in reader:
-            # print(row['id'], row['identifier'])
+    #form the identifier
+    id_separator = '-'
+    identifier = resource['id_0']
+    if 'id_1' in resource.keys():
+        identifier += id_separator + resource['id_1']
+    if 'id_2' in resource.keys():
+        identifier += id_separator + resource['id_2']
+    if 'id_3' in resource.keys():
+        identifier += id_separator + resource['id_3']
+    # MINT the ark
+    if 'ead_location' in resource.keys() and 'ark:' in resource['ead_location']: # Nevermind then.
+        logging.info('Existing ARK for %s (%s): %s' % (rid,identifier,resource['ead_location']))
+        continue
 
-            # Clean collection identifiers if necessary
-            if '[' in row['identifier']: #identifier list straight from DB, let's clean it up
-                identifier = ''
-                #join chokes on unused identifier fields (null values), loop instead
-                for f in json.loads(row['identifier']):
-                    # concatenate but don't include null fields
-                    identifier += ( f + '-' ) if f else ''
+    dublin_core = {}
+    dublin_core['_profile'] = 'dc'
+    if 'finding_aid_title' in resource.keys() and resource['finding_aid_title']:
+        dublin_core['title'] = resource['finding_aid_title']
+    elif 'title' in resource.keys() and resource['title']:
+        dublin_core['title'] = resource['title']
+    dublin_core['type'] = 'finding aids'
+    dublin_core['creator'] = 'UNLV University Libraries'
+    dublin_core['publisher'] = 'UNLV University Libraries'
+    dublin_core['relation'] = 'collection number %s' % identifier
+    ark = '%s/%s' % (config.get('ezid','ark-resolver'),mint_ark(identifier, dublin_core))
 
-                #pull off the extraneous hyphens and return
-                row['identifier'] = identifier.rstrip('-')
-                logging.info('Cleaned identifier for %s: %s' % (row['id'], row['identifier']))
+    logging.info('Updating the EAD Location for %s' % rid)
+    resource['ead_location'] = ark
+    archivesspace_api_call('/repositories/%s/resources/%s' % (
+                config.get('archivesspace', 'repository'),rid),
+            'POST', json.dumps(resource))
 
-            # MINT the ark
-            if 'ark:' in row['ead_location']:
-                ark = row['ead_location']
-                logging.info('Existing ARK for %s: %s' % (row['identifier'],ark))
-            else:
-                who_what_when = {}
-                if 'title' in row.keys() and row['title']:
-                    who_what_when['what'] = row['title']
-                if 'creator' in row.keys() and row['creator']:
-                    who_what_when['who'] = row['creator']
-                if 'dates' in row.keys() and row['dates']:
-                    who_what_when['when'] = row['dates']
-                ark = '%s/%s' % (config.get('ezid','ark-resolver'),mint_ark(row['identifier'], who_what_when))
+    logging.info('Generating the PDF for %s (%s)' % (rid,identifier))
+    if not os.path.isdir(config.get('pdf','export-location')):
+        os.makedirs(os.path.normpath(config.get('pdf','export-location')))
 
-            #PRINT id, identifier, and ARK
-            print('\t'.join([row['id'],row['identifier'],ark]))
-
-            # UPDATE ArchivesSpace EAD Location
-            update_ead_location(row['id'],ark)
-
-            # EXPORT PDF
-            export_fa_pdf(row['id'],row['identifier'])
-
-    ## Testing API Calls so we don't have to rely on a spreadsheet export...
-    ## Could use /repositories/:id/resources but I would rather not retrieve every resource just to find the ones I want...
-    ## Search narrows down the results to published (or not) resources, but doesn't like searching ead_location...
-    ## Bah, long live SQL queries to TSV.
-
-    # print(json.dumps(archivesspace_api_call("/repositories/%s/resources" % config.get('archivesspace', 'repository'), 'GET', {"page": '1', 'page_size':'2'}), indent = 2))
-    # print(json.dumps(archivesspace_api_call("/search", data = {'page':'1','type[]':'resource','aq':json.dumps({'query':{'field':'title','value':'Test','jsonmodel_type':'field_query'}})}), indent = 2))
-    # print(json.dumps(archivesspace_api_call("/search",
-    #     data = {'page':'1', 'page_size':'1',
-    #             'type[]':'resource',
-    #             'aq':json.dumps({'query':
-    #                                 {'op':'AND',
-    #                                 'subqueries':[
-    #                                     {'field':'title','value':'Test','jsonmodel_type':'field_query'},
-    #                                     {'field':'published','value':'true','jsonmodel_type':'boolean_field_query'}],
-    #                                     ],
-    #                                 'jsonmodel_type':'boolean_query'
-    #                                 },
-    #                               'jsonmodel_type':'advanced_query'
-    #                             })
-    #             }), indent = 2))
-    # print(json.dumps(archivesspace_api_call("/search",
-    #             data = {'page':'1','page_size':'1',
-    #                     'type[]':'resource',
-    #                     'aq':json.dumps(
-    #                             {'query':
-    #                                 {'field':'published',
-    #                                 'value':'true',
-    #                                 'jsonmodel_type':'boolean_field_query'
-    #                                 }
-    #                             }
-    #                     )
-    #             }
-    #         ),indent = 2)
-    # )
+    with open(os.path.normpath('%s/%s.pdf' % (config.get('pdf','export-location'), identifier )), "wb") as local_file:
+        local_file.write(archivesspace_api_call("/repositories/%s/resource_descriptions/%s.pdf" % (config.get('archivesspace', 'repository'), rid ), as_obj = False).read())
