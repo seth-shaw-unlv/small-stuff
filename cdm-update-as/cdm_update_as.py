faecfc1723fbfbd52c7333e9cde6002203a000b5
Seth Shaw <seth.shaw@unlv.edu>
2018-05-01 14:12:28 -0700

    always create AS DOs and account for dates
    - adds checks for dates if necessary to disambiguate
    - adds dates to AS DOs when available
    - always creates an AS DO if one doesn't exist
    - checks if AO already linked to AS DO in question

--- cdm-update-as/cdm_update_as.py
+++ cdm-update-as/cdm_update_as.py
@@ -133,6 +133,8 @@ if __name__ == '__main__':
     parser.add_argument("-f","--collection-id-field", help='the CONTENTdm field storing the source collection\'s identifier (overrides config.ini)')
     parser.add_argument("-k","--ark-field", help='the CONTENTdm field to use for the object ARKs (overrides config.ini)')
     parser.add_argument("-o","--digital-object-field", help='the CONTENTdm field to use for the digital object\'s identifier (overrides config.ini)')
+    parser.add_argument("-r","--date-range-field", help='the CONTENTdm field storing digital object date ranges')
+    parser.add_argument("-s","--date-single-field", help='the CONTENTdm field storing digital object single dates')
     parser.add_argument("-v","--verbosity", help="change output verbosity: DEBUG, INFO (default), ERROR")
     parser.add_argument("-d","--dry", help="doesn't update ArchivesSpace, used for testing", action="store_true")
     args = parser.parse_args()
@@ -183,6 +185,18 @@ if __name__ == '__main__':
         do_field = args.digital_object_field
     cdm_fields.append(do_field)
 
+    # Which field holds the Digital Object Date Range?
+    dr_field = config.get('cdm','date-range-field')
+    if args.date_range_field:
+        dr_field = args.date_range_field
+    cdm_fields.append(dr_field)
+
+    # Which field holds the Digital Object Signle Date?
+    ds_field = config.get('cdm','date-single-field')
+    if args.date_single_field:
+        ds_field = args.date_single_field
+    cdm_fields.append(ds_field)
+
     # Limit by AS collection id?
     query = '0'
     if args.collection_id:
@@ -212,7 +226,7 @@ if __name__ == '__main__':
                 resources = aspace_client.api_call('/repositories/%s/search?page=1&aq=%s' % (repository,resource_query))
                 #Sometimes we get bad identifiers from CDM, make sure what we get it good, or report back.
                 if not resources['results']:
-                    logging.info('SKIPPING: Collection ID %s not found in ArchivesSpace for %s' % (result[cid_field],result['pointer']))
+                    logging.warn('SKIPPING: Collection ID %s not found in ArchivesSpace for %s' % (result[cid_field],result['pointer']))
                     current_as_rid = None
                     continue
                 # Take the first result uri
@@ -221,53 +235,147 @@ if __name__ == '__main__':
 
             # REPORT and SKIP if the do has no ARK
             if ark_field and not result[ark_field]:
-                logging.info('SKIPPING: no ARK for %s/id/%d (%s:%s)' % (result['collection'],result['pointer'],current_cid,current_as_rid))
+                logging.warn('SKIPPING: no ARK for %s/id/%d (%s:%s)' % (result['collection'],result['pointer'],current_cid,current_as_rid))
                 continue
             #  REPORT and SKIP if the do has no title
             if not 'title' in result:
-                logging.info('SKIPPING: no Title for %s/id/%d' % (result['collection'],result['pointer']))
+                logging.warn('SKIPPING: no Title for %s/id/%d' % (result['collection'],result['pointer']))
                 continue
             #  REPORT and SKIP if the do has no collection identifier
             if not cid_field in result:
-                logging.info('SKIPPING: no Collection ID (%s) for %s/id/%d' % (cid_field,result['collection'],result['pointer']))
+                logging.warn('SKIPPING: no Collection ID (%s) for %s/id/%d' % (cid_field,result['collection'],result['pointer']))
                 continue
+
+            # For CDM date for matching and AS DO creation
+            cdm_obj_date = ''
+            as_do_date_obj = {}
+            if ds_field in result and result[ds_field]:
+                cdm_obj_date = result[ds_field]
+                if not 'undated' in cdm_obj_date:
+                    as_do_date_obj['date_type'] = 'single'
+                    as_do_date_obj['begin'] = cdm_obj_date
+            elif dr_field in result and result[dr_field]:
+                date_range_array = result[dr_field].split('; ')
+                cdm_obj_date = '%s-%s' % (date_range_array[0],date_range_array[-1])
+                as_do_date_obj['date_type'] = 'inclusive'
+                as_do_date_obj['begin'] = date_range_array[0]
+                as_do_date_obj['end'] = date_range_array[-1]
+
+            # CREATE the AS Digital Object
+            as_do = {'title':result['title'],
+                     'digital_object_id':result[do_field],
+                     'publish':True,
+                     'file_versions':[{'file_uri':result[ark_field],
+                                       'publish':True,
+                                       'is_representative':True}],
+                     'dates':[]}
+
+            if as_do_date_obj:
+                as_do_date_obj['label'] = 'creation'
+                as_do['dates'].append(as_do_date_obj)
+
+            as_do_query = '{"query":{"op":"AND","subqueries":[{"field":"digital_object_id","value":"%s","jsonmodel_type":"field_query","negated":false,"literal":true},{"field":"primary_type","value":"digital_object","jsonmodel_type":"boolean_field_query"}],"jsonmodel_type":"boolean_query"},"jsonmodel_type":"advanced_query"}' % (urllib.quote_plus(result[do_field].replace('\n', '').replace('\r', '')))
+            as_do_response = aspace_client.api_call('/repositories/%s/search?page=1&aq=%s' % (repository,as_do_query))
+
+            as_do_uri = ''
+            if as_do_response['total_hits'] == 0:
+                # Make a new AS DO
+                as_do_uri = 'FAKE/URI' # Incase the DRY option is enabled
+                if dry:
+                    logging.info('DRY: Would create AS digital object: %s' % (json.dumps(as_do)))
+                else:
+                    as_do_response = aspace_client.api_call('/repositories/%s/digital_objects' % (repository),'POST', as_do)
+                    if not 'uri' in as_do_response:
+                        logging.warn('FAILED to create AS digital object %s %s: %s' % ('/repositories/%s/digital_objects' % (repository), as_do, json.dumps(as_do_response)))
+                        continue
+                    as_do_uri = as_do_response['uri']
+            else:
+                as_do_uri = as_do_response['results'][0]['uri']
+                logging.debug('FOUND: AS DO for %s/id/%s: %s' % (alias,result['pointer'],as_do_uri))
 
             # Query ArchivesSpace for archival objects (ao) with matching title and collection URI (root record)
             ao_query = '{"query":{"op":"AND","subqueries":[{"field":"title","value":"%s","jsonmodel_type":"field_query","negated":false,"literal":true},{"field":"primary_type","value":"archival_object","jsonmodel_type":"boolean_field_query"}],"jsonmodel_type":"boolean_query"},"jsonmodel_type":"advanced_query"}' % (urllib.quote_plus(result['title'].replace('"','\\"').replace('\n', '').replace('\r', '')))
             archival_objects = aspace_client.api_call('/repositories/%s/search?page=1&aq=%s&root_record=%s' % (repository,ao_query,current_as_rid))
 
             if not archival_objects['results']:
-                logging.info('SKIPPING: Could not find title "%s" in resource %s for %s/id/%s' % (result['title'],current_as_rid,alias,result['pointer']))
+                logging.warn ('SKIPPING: Could not find title "%s" in resource %s for %s/id/%s (%s)' % (result['title'],current_as_rid,alias,result['pointer'],as_do_uri))
                 continue
             # Check to see if we have different URIs, or multiple of the same
-            ao_uris = list()
+            ao_objs = list()
             for ao in archival_objects['results']:
-                ao_uris.append(ao['uri'])
-            ao_uris = set(ao_uris)
-            if len(ao_uris) > 1:
-                logging.info('SKIPPING: multiple Archival Objects with title "%s" in resource %s for %s/id/%s: %s'% (result['title'],current_as_rid,alias,result['pointer'],','.join(ao_uris)))
-                continue
-            # Build a clean copy from search results json field
-            archival_object = json.loads(archival_objects['results'][0]['json'])
-            # - IF a single result: update ao in AS with instance link using the ARK
-            # based on https://github.com/djpillen/bentley_scripts/blob/master/update_archival_object.py
-            ado = {'title':result['title'],'digital_object_id':result[do_field],'publish':True,'file_versions':[{'file_uri':result[ark_field],'publish':True,'is_representative':True}]}
-            ado_uri = 'FAKE/URI' # Incase the DRY option is enabled
-            if dry:
-                logging.debug('DRY: Would create AS digital object: %s' % (json.dumps(ado)))
-            else:
-                ado_response = aspace_client.api_call('/repositories/%s/digital_objects' % (repository),'POST', ado)
-                if not 'uri' in ado_response:
-                    logging.warn('FAILED to create AS digital object %s %s: %s' % ('/repositories/%s/digital_objects' % (repository), ado, json.dumps(ado_response)))
-                    continue
-                ado_uri = ado_response['uri']
+                ao_obj = json.loads(ao['json'])
+                # Have we seen this object in the results before?
+                found_existing_uri = False
+                for existing_ao in ao_objs:
+                    if existing_ao['uri'] == ao_obj['uri']:
+                        found_existing_uri = True
+                        continue
+                if not found_existing_uri:
+                    ao_objs.append(ao_obj)
 
+            if len(ao_objs) > 1:
+                # Check to see if we get a date match
+                date_matches = []
+                for ao in ao_objs:
+                    found_matching_date = False
+                    for ao_date in ao['dates']:
+                        ao_date_expression = ao_date['expression']
+                        # AS has some decade expressions that don't play nice (e.g. 1950s and 1950s-1960s)
+                        if 's' in ao_date_expression: #decade expression
+                            date_parts = ao_date_expression.split('-')
+                            if len(date_parts) > 1: # decade range
+                                first_decade = date_parts[0].strip('s')
+                                second_decade = date_parts[1]
+                                if second_decade.endswith('s'):
+                                    second_decade = '%s9' % (second_decade.strip('s')[:-1])
+                                ao_date_expression = '-'.join((first_decade,second_decade))
+                            else: #single decade
+                                decade = ao_date_expression.strip('0s')
+                                ao_date_expression = '%s0-%s9' % (decade,decade)
+
+                        if cdm_obj_date == ao_date_expression :
+                            found_matching_date = True
+                    if found_matching_date:
+                        date_matches.append(ao)
+
+                if len(date_matches) > 1:
+                    result_uris = []
+                    for ao in date_matches:
+                        result_uris.append(ao['uri'])
+                    logging.warn('SKIPPING: multiple Archival Objects with title "%s" and matching dates (%s) in resource %s for %s/id/%s (%s): %s'% (result['title'],cdm_obj_date,current_as_rid,alias,result['pointer'],as_do_uri,','.join(result_uris)))
+                    continue
+                elif len(date_matches) < 1:
+                    result_uris = []
+                    for ao in ao_objs:
+                        result_uris.append(ao['uri'])
+                    logging.warn('SKIPPING: multiple Archival Objects with title "%s" in resource %s for %s/id/%s (%s): %s'% (result['title'],current_as_rid,alias,result['pointer'],as_do_uri,','.join(result_uris)))
+                    continue
+                elif len(date_matches) == 1: # Only one matches title and date, reset the result list to match
+                    ao_objs = date_matches
+
+            # Get a cleaner object reference to use
+            as_ao = ao_objs[0]
+            logging.debug('MATCH: Archival Object "%s" (%s) matches CDM "%s" date: %s (%s/id/%s)' % (as_ao['display_string'].replace('\n', '').replace('\r', ''),as_ao['uri'],result['title'],cdm_obj_date,alias,result['pointer']))
+
+            # If the AS AO already has a DO, we assume it is the same match we just identified.
+            has_do = False
+            for instance in as_ao['instances']:
+                if not instance['instance_type'] == 'digital_object':
+                    continue
+                if  instance['digital_object']['ref'] == as_do_uri:
+                    has_do = True
+                    continue
+            if has_do:
+                logging.info('SKIPPING: Archival Object %s already linked to %s'% (as_ao['uri'],as_do_uri))
+                continue
+
+            # Update ao in AS with instance link using the ARK
+            # based on https://github.com/djpillen/bentley_scripts/blob/master/update_archival_object.py
             # Update the Archival Object
-            # TODO: (Assuming we ignore that the AS digital object already existed.) Add checking to make sure we don't already have a matching instance
-            ado_instance = {'instance_type':'digital_object','digital_object':{'ref':ado_uri}}
-            archival_object['instances'].append(ado_instance)
+            as_do_instance = {'instance_type':'digital_object','digital_object':{'ref':as_do_uri}}
+            as_ao['instances'].append(as_do_instance)
             if dry:
-                logging.info('DRY: Would update AS Archival Object %s with new instance %s' % (archival_object['uri'], ado_instance))
+                logging.info('DRY: Would update AS Archival Object %s with new instance %s' % (as_ao['uri'], as_do_instance))
             else:
-                ao_update_response = aspace_client.api_call(archival_object['uri'],'POST', archival_object)
-                logging.info('%s: AS Archival Object %s with AS Digital Object %s for CDM object %s : %s' % (ao_update_response['status'],ao_update_response['id'],ado_uri,result[do_field],result['title']))
+                ao_update_response = aspace_client.api_call(as_ao['uri'],'POST', as_ao)
+                logging.info('%s: AS Archival Object %s with AS Digital Object %s for CDM object %s : %s' % (ao_update_response['status'],ao_update_response['id'],as_do_uri,result[do_field],result['title']))
