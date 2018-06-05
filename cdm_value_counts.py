0b7dd43fcb4f5c426ed195c801646d6b4fa69390
Seth Shaw <seth.shaw@unlv.edu>
2018-06-05 13:01:05 -0700

    initial cmd_value_counts

---
+++ cdm_value_counts.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+import CDM
+import os, sys, logging, csv
+import json
+
+if __name__ == '__main__':
+
+    ws_url='https://yourserver.contentdm.oclc.org'
+    queryClient = CDM.QueryClient(ws_url)
+    # Check arguments
+    if len(sys.argv)<2:
+        sys.exit("Please provide at least one CDM alias to query.")
+
+    query='0' # get every item
+    fields = ['date','creato','covera','narrat']
+    for alias in sys.argv[1:]:
+        value_counts = {}
+        for result in queryClient.query(alias,query,fields):
+
+            for field in fields:
+                # Nothing to see here?
+                if (field not in result) or not result[field]:
+                    continue
+
+                # Have we seen this field yet?
+                if not field in value_counts:
+                    value_counts[field] = {}
+
+                for value in result[field].split(';'):
+                    value = value.strip()
+
+                    # A blank got through, skip it.
+                    if not value:
+                        continue
+
+                    # Have we seen this value in this field yet?
+                    if not value in value_counts[field]:
+                        value_counts[field][value] = 0;
+
+                    # Tally
+                    value_counts[field][value] += 1
+
+        for field, value_count in value_counts.items():
+            for value, count in value_count.items():
+                print("\t".join((alias,field,value,str(count))))
