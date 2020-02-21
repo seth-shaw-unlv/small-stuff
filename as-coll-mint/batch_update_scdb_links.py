141f28a176182fae8b2a7201aa4a8b171c142dc2
Seth Shaw <seth.shaw@unlv.edu>
2020-02-21 08:37:15 -0800

    use CLI switch for batch_update_scdb_links

--- as-coll-mint/batch_update_scdb_links.py
+++ as-coll-mint/batch_update_scdb_links.py
@@ -5,21 +5,45 @@ import sys
 
 # Only works with MSS table. Update for each table.
 
-TABLE = 'SPEC_MAN_MANUSCRIPTS_MAN'
-ID = 'id_man'
+TABLE = 'SPEC_PC_PHOTOCOLL_PHO'
+ID = 'id_pho'
 IDENTIFIER = 'coll_man'
 LINK = 'link_guide'
 
-# SPEC_MAN_MANUSCRIPTS_MAN, id_man, coll_man, link_guide
-# SPEC_ORAL_HISTORY_SOH, id_soh, coll_number_soh, link_guide_soh, link_digital_soh
-# SPEC_PC_PHOTOCOLL_PHO, id_pho, coll_number_pho, link_guide_pho
-# SPEC_UNLV_ARCHIVES_UAR, id_uar, rec_grp_num, link_guide_uar
-
-# UA REC CSV export uses 'Link to Collection Guide' instead of 'Link to Guide'.
-CSV_LINK_FIELD = 'Link to Guide'
-
-# UA REC CSV export uses 'Record Group Number' instead of 'Coll #'.
-CSV_COLL_FIELD = 'Coll #'
+fields = {
+  'man': {
+    'table': 'SPEC_MAN_MANUSCRIPTS_MAN',
+    'pk': 'id_man',
+    'collection_id': 'coll_man',
+    'ark_column': 'link_guide',
+    'csv_ark_field': 'Link to Guide',
+    'csv_coll_field': 'Coll #',
+  },
+  'pho': {
+    'table': 'SPEC_PC_PHOTOCOLL_PHO',
+    'pk': 'id_pho',
+    'collection_id': 'coll_number_pho',
+    'ark_column': 'link_guide_pho',
+    'csv_ark_field': 'Link to Guide',
+    'csv_coll_field': 'Coll #',
+  },
+  'ua': {
+    'table': 'SPEC_UNLV_ARCHIVES_UAR',
+    'pk': 'id_uar',
+    'collection_id': 'rec_grp_num',
+    'ark_column': 'link_guide_uar',
+    'csv_ark_field': 'Link to Collection Guide',
+    'csv_coll_field': 'Record Group Number',
+  },
+  'oh': {
+    'table': 'SPEC_ORAL_HISTORY_SOH',
+    'pk': 'id_soh',
+    'collection_id': 'coll_number_soh',
+    'ark_column': 'link_guide_soh',
+    'csv_ark_field': 'Link to Guide',
+    'csv_coll_field': 'Coll #',
+  },
+}
 
 # ArchivesSpace export
 # export the query `select id, title, identifier, ead_location from resource where publish is True AND ead_location IS NOT Null;`
@@ -28,28 +52,29 @@ CSV_COLL_FIELD = 'Coll #'
 if __name__ == '__main__':
 
     # Ensure we have the two arguments
-    if not len(sys.argv) > 2:
-        print 'Please provide both the archivesspace export (from db) and scdb export (from admin interface)'
+    if not len(sys.argv) > 3:
+        print 'Please provide the type-key, archivesspace export (from db), and scdb export (from admin interface)'
         exit();
 
     # load up an identifier associative array storing arks for guides
     identifier_ark = {}
-    with open(sys.argv[1]) as csvfile:
+    table_settings = fields[sys.argv[1]]
+    with open(sys.argv[2]) as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
             identifier_ark[row['identifier']] = row['ead_location']
 
     # Run through the DB entries and create update statements
     update_sql = ''
-    with open(sys.argv[2], 'rU') as csvfile:
+    with open(sys.argv[3], 'rU') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='|')
         for r in reader:
-            if r[CSV_LINK_FIELD].find('ark:') > -1:
-		print(r[CSV_COLL_FIELD]+' already has an ark '+r[CSV_LINK_FIELD])
+            if r[table_settings['csv_ark_field']].find('ark:') > -1:
+		print(r[table_settings['csv_coll_field']]+' already has an ark '+r[table_settings['csv_ark_field']])
                 continue
-            if r[CSV_COLL_FIELD] in identifier_ark.keys():
-                update_sql += "UPDATE {0} SET {1}='{2}' WHERE {3}='{4}';\n".format(TABLE, LINK, identifier_ark[r[CSV_COLL_FIELD]].strip(), ID, r['id'])
+            if r[table_settings['csv_coll_field']] in identifier_ark.keys():
+                update_sql += "UPDATE {0} SET {1}='{2}' WHERE {3}='{4}';\n".format(table_settings['table'], table_settings['ark_column'], identifier_ark[r[table_settings['csv_coll_field']]].strip(), table_settings['pk'], r['id'])
             else:
-                print(r[CSV_COLL_FIELD]+' is not in ArchivesSpace')
+                print(r[table_settings['csv_coll_field']]+' is not in ArchivesSpace')
 
     print(update_sql);
